d333b4a64f79d3fae18dad65483be46bf76aa67e
Gustavo Lopes <gustavo.lopesv3@gmail.com>
2020-06-14 09:47:41 -0500

    animation colors

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:alcool_ou_gasolina/widgets/logo.widget.dart';
 import 'package:alcool_ou_gasolina/widgets/submit-form.dart';
 import 'package:alcool_ou_gasolina/widgets/success.widget.dart';
@@ -13,21 +15,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Color _color = Colors.deepPurple;
   var _maskGas = new MoneyMaskedTextController();
-
   var _maskAlc = new MoneyMaskedTextController();
-
   var _busy = false;
-
   var _completed = false;
-
   var _resultText = 'compensa utlizar alcool';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
+      body: AnimatedContainer(
+        duration: Duration(milliseconds: 1200),
+        color: _color,
+        child: ListView(
         children: <Widget>[
           Logo(),
           _completed
@@ -44,6 +46,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      )
     );
   }
 
@@ -55,12 +58,13 @@ class _HomePageState extends State<HomePage> {
     double res = alc / gas;
 
     setState(() {
+      _color = Colors.red;
      _completed = false;
      _busy = true; 
     });
 
   return new Future.delayed(
-    const Duration(seconds: 3),
+    const Duration(seconds: 1),
     () => {
           setState(() {
           if (res >= 0.7) {
@@ -76,6 +80,7 @@ class _HomePageState extends State<HomePage> {
   }
     reset() {
       setState(() {
+        _color = Colors.deepPurple;
        _maskGas = new MoneyMaskedTextController();
         _maskAlc = new MoneyMaskedTextController();
         _completed = false;
