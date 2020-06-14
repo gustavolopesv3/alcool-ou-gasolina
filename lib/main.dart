98b92c0403c42b75dd04d18b084ffd9dc8029829
Gustavo Lopes <gustavo.lopesv3@gmail.com>
2020-06-13 20:10:35 -0500

    app

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:alcool_ou_gasolina/widgets/logo.widget.dart';
+import 'package:alcool_ou_gasolina/widgets/submit-form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Alcool ou Gasolina',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  var _maskGas = new MoneyMaskedTextController();
+  var _maskAlc = new MoneyMaskedTextController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+          SubmitForm(
+            maskAlc: _maskAlc,
+            maskGas: _maskGas,
+            submitFunc: (){},
+            bisy: false,
+
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/submit-form.dart
@@ -0,0 +1,55 @@
+import 'package:alcool_ou_gasolina/widgets/input.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+import 'loading-button.widget.dart';
+
+class SubmitForm extends StatelessWidget {
+  var maskGas = new MoneyMaskedTextController();
+  var maskAlc = new MoneyMaskedTextController();
+  var bisy = false;
+  Function submitFunc;
+
+  SubmitForm({
+    @required this.maskGas,
+    @required this.maskAlc,
+    @required this.bisy,
+    @required this.submitFunc,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(
+            left: 30,
+            right: 30,
+          ),
+          child: Input(
+            label: "Gasolina",
+            ctrl: maskGas,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            left: 30,
+            right: 30,
+          ),
+          child: Input(
+            label: "Alcool",
+            ctrl: maskGas,
+          ),
+        ),
+
+        LoadingButton(
+            bisy: bisy,
+            func: submitFunc,
+            text: 'CALCULAR',
+            invert: false,
+          ),
+      ],
+      
+    );
+  }
+}
